f8e7a5d96a920d44f243e8b604e0cb2915a6a87d
James Starkman <james@starkman.net>
2024-10-13 14:11:59 -0400

    Found it like this three years later

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,37 @@
+use std::path::Path;
+
+use actix_http::error::ErrorBadRequest;
 use actix_multipart::Multipart;
-use actix_web as aw;
-use actix_web::{delete, get, post, web, App, HttpResponse, HttpServer, Responder};
-use futures::{StreamExt, TryStreamExt};
+use actix_web::{
+	delete, get, middleware::Logger, post, App, HttpResponse, HttpServer, Responder,
+};
 use log::{debug, info};
 use serde::Deserialize;
 use uuid::Uuid;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct HelloInputs {
 	name: String,
 }
 
 #[get("/{name}")]
-async fn hello(inputs: web::Path<HelloInputs>) -> impl Responder {
+async fn hello(inputs: actix_web::web::Path<HelloInputs>) -> impl Responder {
 	debug!("Called hello.");
 	HttpResponse::Ok().body(format!("Hello, {}!", inputs.name))
 }
 
 #[get("/upload/{id}")]
-async fn upload_get(id: web::Path<String>) -> Result<HttpResponse, aw::Error> {
+async fn upload_get(
+	id: actix_web::web::Path<String>,
+) -> Result<HttpResponse, actix_web::Error> {
 	// FIXME should store/recover filename/MIME somehow; proper DB?
-	use actix_http::error::ErrorBadRequest;
-	use std::path::Path;
 	let uuid: Uuid = Uuid::parse_str(&id).map_err(ErrorBadRequest)?;
-	// FIXME this should go in application state/config; live env lookup could be expensive
-	let tfs_upload = std::env::var("TFS_UPLOAD").unwrap_or("./upload".to_string());
-	let read_from_here = Path::new(&tfs_upload).join(uuid.to_hyphenated().to_string());
+	// FIXME this should go in application state/config;
+	// live env lookup could be expensive
+	let tfs_upload =
+		std::env::var("TFS_UPLOAD").unwrap_or_else(|_| "./upload".to_string());
+	let read_from_here =
+		Path::new(&tfs_upload).join(uuid.to_hyphenated().to_string());
 	// FIXME stream this
 	let data = std::fs::read(read_from_here).map_err(ErrorBadRequest)?;
 	Ok(HttpResponse::Ok()
@@ -34,13 +40,15 @@ async fn upload_get(id: web::Path<String>) -> Result<HttpResponse, aw::Error> {
 }
 
 #[delete("/upload/{id}")]
-async fn upload_delete(id: web::Path<String>) -> Result<HttpResponse, aw::Error> {
+async fn upload_delete(
+	id: actix_web::web::Path<String>,
+) -> Result<HttpResponse, actix_web::Error> {
 	// FIXME should store/recover filename/MIME somehow; proper DB?
-	use actix_http::error::ErrorBadRequest;
-	use std::path::Path;
 	let uuid: Uuid = Uuid::parse_str(&id).map_err(ErrorBadRequest)?;
-	// FIXME this should go in application state/config; live env lookup could be expensive
-	let tfs_upload = std::env::var("TFS_UPLOAD").unwrap_or("./upload".to_string());
+	// FIXME this should go in application state/config;
+	// live env lookup could be expensive
+	let tfs_upload =
+		std::env::var("TFS_UPLOAD").unwrap_or_else(|_| "./upload".to_string());
 	let delete_me = Path::new(&tfs_upload).join(uuid.to_hyphenated().to_string());
 	std::fs::remove_file(delete_me).map_err(ErrorBadRequest)?;
 	Ok(HttpResponse::NoContent().finish())
@@ -49,20 +57,23 @@ async fn upload_delete(id: web::Path<String>) -> Result<HttpResponse, aw::Error>
 /// Assumes MIME of "multipart/form-data" with names of "identifier" and "f".
 /// @see https://github.com/actix/examples/blob/master/forms/multipart/src/main.rs
 #[post("/upload")]
-async fn upload_post(mut multipart: Multipart) -> Result<HttpResponse, aw::Error> {
-	use actix_http::error::ErrorBadRequest;
+async fn upload_post(
+	mut multipart: Multipart,
+) -> Result<HttpResponse, actix_web::Error> {
 	let mut identifier: Option<String> = None;
 	let mut filename: Option<String> = None;
 	let mut filepath: Option<String> = None;
 	// https://docs.rs/futures/0.3/futures/stream/trait.TryStreamExt.html#method.try_next
+	use futures::TryStreamExt;
 	while let Ok(Some(mut field)) = multipart.try_next().await {
 		// FIXME handle errors
-		let content_disposition = field
-			.content_disposition()
-			.ok_or(ErrorBadRequest("Malformed multipart/form-data header."))?;
+		let content_disposition =
+			field.content_disposition().ok_or_else(|| {
+				ErrorBadRequest("Malformed multipart/form-data header.")
+			})?;
 		let name = content_disposition
 			.get_name()
-			.ok_or(ErrorBadRequest("Missing multipart name."))?;
+			.ok_or_else(|| ErrorBadRequest("Missing multipart name."))?;
 		match name {
 			"f" => {
 				filename = content_disposition
@@ -71,11 +82,11 @@ async fn upload_post(mut multipart: Multipart) -> Result<HttpResponse, aw::Error
 				filepath = Some(stream_to_file(field).await?);
 			}
 			"identifier" => {
+				use futures::StreamExt;
 				identifier = match field.next().await {
-					Some(r) => match r {
-						Ok(bytes) => String::from_utf8(bytes.to_vec()).ok(),
-						_ => None,
-					},
+					Some(Ok(bytes)) => {
+						String::from_utf8(bytes.to_vec()).ok()
+					}
 					_ => None,
 				};
 			}
@@ -91,7 +102,10 @@ async fn upload_post(mut multipart: Multipart) -> Result<HttpResponse, aw::Error
 			let loc = format!("/api/upload/{}", fpath);
 			Ok(HttpResponse::Created()
 				// FIXME deprecated; will change in actix-web >= 4.0.0
-				.header(aw::http::header::LOCATION, loc.to_string())
+				.header(
+					actix_web::http::header::LOCATION,
+					loc.to_string(),
+				)
 				.body(format!("Uploaded to {}", loc)))
 		}
 		_ => Ok(HttpResponse::from_error(ErrorBadRequest(
@@ -100,18 +114,24 @@ async fn upload_post(mut multipart: Multipart) -> Result<HttpResponse, aw::Error
 	}
 }
 
-async fn stream_to_file(mut field: actix_multipart::Field) -> Result<String, aw::Error> {
+async fn stream_to_file(
+	mut field: actix_multipart::Field,
+) -> Result<String, actix_web::Error> {
 	use std::fs::File;
 	use std::io::Write;
-	use std::path::Path;
-	// FIXME this should go in application state/config; live env lookup could be expensive
-	let tfs_upload = std::env::var("TFS_UPLOAD").unwrap_or("./upload".to_string());
+	// FIXME this should go in application state/config;
+	// live env lookup could be expensive
+	let tfs_upload =
+		std::env::var("TFS_UPLOAD").unwrap_or_else(|_| "./upload".to_string());
 	let filename = Uuid::new_v4().to_hyphenated().to_string();
 	let write_here = Path::new(&tfs_upload).join(&filename);
-	let mut f: File = aw::web::block(|| File::create(write_here)).await.unwrap();
+	let mut f: File = actix_web::web::block(|| File::create(write_here))
+		.await
+		.unwrap();
+	use futures::StreamExt;
 	while let Some(chunk) = field.next().await {
-		let data: aw::web::Bytes = chunk.unwrap();
-		f = aw::web::block(move ||  // move to take ownership of `data`
+		let data: actix_web::web::Bytes = chunk.unwrap();
+		f = actix_web::web::block(move ||  // move to take ownership of `data`
 			f.write_all(&data).map(|_| f))
 		.await?;
 	}
@@ -119,21 +139,26 @@ async fn stream_to_file(mut field: actix_multipart::Field) -> Result<String, aw:
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> color_eyre::Result<(), color_eyre::Report> {
 	dotenv::dotenv().ok();
 	env_logger::builder().format_timestamp_millis().init();
+	color_eyre::install()?;
 
-	let static_files_root = std::env::var("TFS_STATIC").unwrap_or("static".to_string());
-	let upload_files_root = std::env::var("TFS_UPLOAD").unwrap_or("./upload".to_string());
+	use std::env::var;
+	let static_files_root =
+		var("TFS_STATIC").unwrap_or_else(|_| "static".to_string());
+	let upload_files_root =
+		var("TFS_UPLOAD").unwrap_or_else(|_| "./upload".to_string());
 	std::fs::create_dir_all(upload_files_root)?;
 
-	let bindaddr = std::env::var("TFS_BIND_ADDR").unwrap_or("127.0.0.1:8080".to_string());
+	let bindaddr =
+		var("TFS_BIND_ADDR").unwrap_or_else(|_| "127.0.0.1:8080".to_string());
 	HttpServer::new(move || {
 		App::new()
-			.wrap(aw::middleware::Logger::new("%a \"%r\" %s %b %T"))
+			.wrap(Logger::new("%a \"%r\" %s %b %T"))
 			.service(actix_files::Files::new("/static", &static_files_root))
 			.service(
-				web::scope("api")
+				actix_web::web::scope("api")
 					.service(hello)
 					.service(upload_post)
 					.service(upload_get)
@@ -143,5 +168,9 @@ async fn main() -> std::io::Result<()> {
 	})
 	.bind(bindaddr)?
 	.run()
-	.await
+	.await?;
+	Ok(())
 }
+
+// use anyhow::Error
+// Next: https://actix.rs/docs/databases/
